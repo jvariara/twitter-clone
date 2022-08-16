a2f3d6d3b8aa3caa15104728b6e98357df9375f0
Justin Variara <jrvariar@buffalo.edu>
2022-08-16 17:37:06 -0400

    ADD: Widgets Section and design

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import '../css/Widgets.css'
+import React from "react";
+import "../css/Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  TwitterShareButton,
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
-    <div className='widgets'>
-        <h2>Widgets</h2>
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+
+      <div className="widgets__widget--container">
+        <h2>What's Happening</h2>
+
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
